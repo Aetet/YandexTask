3f3776b76048f681a7055f81bd38ec4efcfb8a67
Aetet <Aetetic@gmail.com>
2013-09-07 15:07:35 +0400

    Test task

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// Require chai.js expect module for assertions
+var chai = require('chai');
+
+
+
+// Create a new test suite for our Bank Account
+describe("Bank Account Tests", function() {
+
+    // Define a pending test
+    chai.expect("should exist");
+
+});
